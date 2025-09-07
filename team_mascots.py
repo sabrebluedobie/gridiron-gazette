4d68475d1954c3e5763bc7c0c0e9099306b29497
Melanie Brown <108430645+sabrebluedobie@users.noreply.github.com>
2025-09-07 12:38:51 -0500

    Create team_mascots.py

---
+++ team_mascots.py
@@ -0,0 +1,17 @@
+# team_mascots.py
+
+team_mascots = {
+    # EAST
+    "Annie1235 slayy": "A 10-year-old girl, shoulder-length blonde hair, feisty and a little sneaky.",
+    "Phoenix Blues": "Cropped and docked blue Doberman (Sabre), protective and silly.",
+    "Nana's Hawks": "Large blue hawk, older.",
+    "Jimmy Birds": "Small blond boy about 6, spunky; runs circles around others.",
+    "Kansas City Pumas": "Large pink puma with style and lethal claws.",
+
+    # WEST
+    "Under the InfluWENTZ": "A drunken beer bottle that hiccups and makes everyone laugh.",
+    "DEM BOY’S! 🏆🏆🏆🏆": "30-year-old cowboy, tough but a little clumsy.",
+    "Avondale Welders": "A dirty, hard-working welder.",
+    "THE 💀REBELS💀": "Large dark-haired, bearded knight wielding a sword; gets angry if losing.",
+    "The Champ Big Daddy": "Large biracial (Black/White) man who loves lifting heavy weights, riding a bicycle, and tracking outdoors.",
+}
